09696fd99bfabb6e914152da04e1a524375f200e
egpennington <egpennington@hotmail.com>
2023-12-27 19:02:44 -0600

    added about in navbar and hidden modal to appear on click

--- main.js
+++ main.js
@@ -1,41 +1,42 @@
 import quotes from "./quotes.js";
 
-const authorEl = document.getElementById("author");
-const quoteEl = document.getElementById("quote");
+const authorEl = document.getElementById("author")
+const quoteEl = document.getElementById("quote")
+const aboutEl = document.getElementById("about-el")
 
 document.addEventListener("DOMContentLoaded", () => {
-  const domainItems = document.querySelectorAll(".domain");
-  const resetButton = document.querySelector(".btn");
+  const domainItems = document.querySelectorAll(".domain")
+  const resetButton = document.querySelector(".btn")
 
   // Function to reset the completion status
   const resetDomains = () => {
-    localStorage.setItem("lastResetDate", getCurrentDate());
+    localStorage.setItem("lastResetDate", getCurrentDate())
 
     domainItems.forEach((item) => {
-      item.classList.remove("completed");
-      localStorage.removeItem(item.id); // Remove completion status from localStorage
-    });
+      item.classList.remove("completed")
+      localStorage.removeItem(item.id) // Remove completion status from localStorage
+    })
   };
 
   // Function to get the current date in the format YYYY-MM-DD
   const getCurrentDate = () => {
-    const now = new Date();
-    const year = now.getFullYear();
-    const month = String(now.getMonth() + 1).padStart(2, "0");
-    const day = String(now.getDate()).padStart(2, "0");
-    return `${year}-${month}-${day}`;
+    const now = new Date()
+    const year = now.getFullYear()
+    const month = String(now.getMonth() + 1).padStart(2, "0")
+    const day = String(now.getDate()).padStart(2, "0")
+    return `${year}-${month}-${day}`
   };
 
   // Check if the stored date matches the current date
-  const lastResetDate = localStorage.getItem("lastResetDate");
+  const lastResetDate = localStorage.getItem("lastResetDate")
   if (lastResetDate !== getCurrentDate()) {
-    resetDomains();
+    resetDomains()
   } else {
     // Apply completion status from localStorage
     domainItems.forEach((item) => {
-      const completed = localStorage.getItem(item.id) === "true";
+      const completed = localStorage.getItem(item.id) === "true"
       if (completed) {
-        item.classList.add("completed");
+        item.classList.add("completed")
       }
     });
   }
@@ -43,27 +44,37 @@ document.addEventListener("DOMContentLoaded", () => {
   // Event listener for domain item clicks
   domainItems.forEach((item) => {
     item.addEventListener("click", () => {
-      item.classList.toggle("completed");
+      item.classList.toggle("completed")
       // Update completion status in localStorage
-      localStorage.setItem(item.id, item.classList.contains("completed"));
-    });
-  });
+      localStorage.setItem(item.id, item.classList.contains("completed"))
+    })
+  })
 
   // Event listener for the reset button
   resetButton.addEventListener("dblclick", resetDomains);
-});
+})
+
+aboutEl.addEventListener("click", () => {
+    const aboutModalEl = document.getElementById("about-modal-el")
+    const aboutCloseEl = document.getElementById("about-close-el")
+    aboutModalEl.style.display = "block"
+
+    aboutCloseEl.addEventListener("click", () => {
+      aboutModalEl.style.display = "none"
+    })
+})
 
 // quotes generator
 function generateRandomQuote() {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[randomIndex];
+  const randomIndex = Math.floor(Math.random() * quotes.length)
+  const randomQuote = quotes[randomIndex]
 
   quoteEl.innerHTML = `
         <h1>${randomQuote.quote}</h1>
-        `;
+        `
   authorEl.innerHTML = `
         <h2 class="rt-side">- ${randomQuote.author}</h2>
-        `;
+        `
 }
 
 generateRandomQuote()
